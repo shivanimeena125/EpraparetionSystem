fcf345e10821b24d9918b66f959efc0af93af235
shivanimeena125 <shivanimeena7454@gmail.com>
2024-09-13 13:32:23 +0530

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,37 @@
+package src.E_praperetion;
+import java.util.Scanner;
+import java.sql.*;
+import java.lang.NullPointerException;
+// import java.sql.PreparedStatement;
+public class Registration {
+    public static void register(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\n"+"Enter your name: ");
+        String username=sc.nextLine();
+        System.out.println("\n"+"Enter your password: ");
+        String password=sc.nextLine();
+
+        if(password.length()>8){
+            System.out.println("\n"+"password must be at least 8 characters long! Try again");
+            return;
+        }
+
+        Connection con=connection.getConnection();
+        String sql="insert into users(username,password) values(?,?)";
+        
+        try{
+       
+        
+        PreparedStatement pstmt=con.prepareStatement(sql);
+        // ResultSet rs=pstmt.executeQuery();
+        pstmt.setString(1, username);
+        pstmt.setString(2, password);
+        pstmt.executeUpdate();
+        System.out.println("\n"+"Registration successfully");
+        
+    }
+    catch(SQLException e){
+        e.printStackTrace();
+    }
+}
+}
